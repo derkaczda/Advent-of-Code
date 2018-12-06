d7612421a9f8bc9f3fb136cb4fcd2dc08bc6cb74
dldz <daniel.derkacz@methodpark.de>
2018-12-06 14:38:49 +0100

    changes on day 6 -> not finished yet

--- 2018/cargo_project/src/day6/mod.rs
+++ 2018/cargo_project/src/day6/mod.rs
@@ -45,14 +45,110 @@ fn parseFieldCoordinates(input : String) -> Vec<FieldCoordinate>
     field_coordinates
 }
 
+fn getMaxCoordinates( coordinates : &Vec<FieldCoordinate>) -> Vec<i32>
+{
+    let mut max_value_x : i32 = 0;
+    let mut max_value_y : i32 = 0;
+    for c in coordinates.iter()
+    {
+        if c.coordinate.x > max_value_x{
+            max_value_x = c.coordinate.x;
+        }
+        if c.coordinate.y > max_value_y {
+            max_value_y = c.coordinate.y;
+        }
+    }
+
+    let mut result : Vec<i32> = Vec::new();
+    result.push(max_value_x);
+    result.push(max_value_y);
+
+    result
+}
+
+fn createGrid(max_x : i32, max_y : i32) -> Vec<Point>
+{
+    let mut result : Vec<Point> = Vec::new();
+    for y in 0..max_y {
+
+        for x in 0..max_x {
+            let mut new_point = Point {
+                x: x,
+                y: y,
+            };
+
+            result.push(new_point);
+        }
+    }
+
+    result
+}
+
+fn calcDistanceBetweenPoints(point1 : &Point, point2 : &Point) -> i32
+{
+    let dist_x = point1.x - point2.x;
+    let dist_y = point1.y - point2.y;
+    let distance = dist_x.abs() + dist_y.abs();
+    distance
+}
+
+fn calcDistances(grid : &Vec<Point>, field_coordinates : &mut Vec<FieldCoordinate>)
+{
+    for p in grid.iter()
+    {
+        let mut smallest_distance : i32 = 50000;
+        let mut point_with_smallest_dist_index : usize = 0;
+        let mut shared_point : bool = false;
+
+        let mut index : usize = 0;
+        for fc in field_coordinates.iter()
+        {
+            let distance = calcDistanceBetweenPoints(&p, &fc.coordinate);
+            if distance < smallest_distance
+            {
+                smallest_distance = distance;
+                point_with_smallest_dist_index = index;
+            }
+            else if distance == smallest_distance
+            {
+                shared_point = true;
+                break;
+            }
+            index += 1;
+        }
+
+        if !shared_point
+        {
+            let point = Point {
+                x: p.x,
+                y: p.y,
+            };
+            field_coordinates[point_with_smallest_dist_index].nearPoints.push(point);
+        }
+    }
+
+}
+
+fn getBiggestField(field_coordinates : &Vec<FieldCoordinate>) -> usize
+{
+    let mut field_size : usize = 0;
+    for f in field_coordinates.iter() {
+        if f.nearPoints.len() > field_size {
+            field_size = f.nearPoints.len()
+        }
+    }
+
+    field_size
+}
+
 
 pub fn problem1()
 {
     let file_content = include_str!("./data/input_6_1_tmp.txt").to_string();
-    let field_coordinates = parseFieldCoordinates(file_content);
+    let mut field_coordinates = parseFieldCoordinates(file_content);
+    let max_coords = getMaxCoordinates(&field_coordinates);
+    let grid = createGrid(max_coords[0], max_coords[1]);
+    calcDistances(&grid, &mut field_coordinates);
 
-    for c in field_coordinates.iter()
-    {
-        println!("{}", c.coordinate.x);
-    }
+    println!("biggest field {}", getBiggestField(&field_coordinates));
 }
