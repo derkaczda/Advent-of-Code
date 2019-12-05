d20d27c349d544783554d424b8e4c63f7eda4054
Derkacz <daniel.derkacz.ext@siemens-healthineers.com>
2019-12-05 14:55:39 +0100

    refactoring the intprogram into a class -> not finished yet

--- 2019/day5/intprogram.h
+++ 2019/day5/intprogram.h
@@ -5,23 +5,147 @@
 
 #include <algorithm>
 #include <sstream>
+#include <stack>
 
 #define OPCODE_ADD 1
 #define OPCODE_MUL 2
 #define OPCODE_END 99
+#define OPCODE_SAVE 3
+#define OPCODE_OUTPUT 4
+
+#define MODE_POSITION 0
+#define MODE_IMMEDIATE 1
+
+class IntProgram
+{
+private:
+    struct InstructionParameter
+    {
+        int value;
+        int mode;
+    };
+    struct Instruction
+    {
+        int opcode;
+        InstructionParameter params[3];
+        int parameterSize;
+    };
+public:
+
+    IntProgram(const std::string& filepath)
+    {
+        m_ProgramSource = filepath;
+        m_InstructionPointer = 0;
+        LoadProgram();
+    }
+
+    void Run()
+    {
+        bool programRunning = true;
+        while(programRunning)
+        {
+            LoadNextInstruction();
+
+            if(m_CurrentInstruction.opcode == OPCODE_ADD)
+            {
+                int valueOne = GetParameterValue(m_CurrentInstruction.params[0]);
+                int valueTwo = GetParameterValue(m_CurrentInstruction.params[1]);
+                int savePosition = m_CurrentInstruction.params[2].value;
+                m_Memory[savePosition] = valueOne + valueTwo;
+            }
+            else if(m_CurrentInstruction.opcode == OPCODE_MUL)
+            {
+                int valueOne = GetParameterValue(m_CurrentInstruction.params[0]);
+                int valueTwo = GetParameterValue(m_CurrentInstruction.params[1]);
+                int savePosition = m_CurrentInstruction.params[2].value;
+                m_Memory[savePosition] = valueOne * valueTwo;
+            }
+            else if(m_CurrentInstruction.opcode == OPCODE_END)
+            {
+                programRunning = false;
+                continue;
+            }
+
+            m_InstructionPointer += m_CurrentInstruction.parameterSize;
+        }  
+    }
+
+private:
+
+    void LoadProgram()
+    {
+        std::vector<std::string> lines;
+        bool result = GetFileContent(m_ProgramSource, lines);
+
+        if(!result)
+        {
+            std::cerr << "Reading file went wrong" << std::endl;
+        }
+
+        std::string program = lines[0];
+        std::replace(program.begin(), program.end(), ',', ' ');
+
+        std::stringstream ss(program);
+        int temp;
+        while(ss >> temp)
+            m_Memory.push_back(temp);
+    }
+
+    std::vector<int> SplitNumber(int number)
+    {
+        std::vector<int> splitDigits;
+        while(number > 0)
+        {
+            int digit = number % 10;
+            number /= 10;
+            splitDigits.push_back(digit);
+        }
+        return splitDigits;
+    }
+
+    void LoadNextInstruction()
+    {
+        auto splitDigits = SplitNumber(m_Memory[m_InstructionPointer]);
+        for(int i = splitDigits.size(); i > 0; i--)
+        {
+            
+        }
+    }
+
+    int GetParameterValue(InstructionParameter parameter)
+    {
+        if(parameter.mode == MODE_IMMEDIATE)
+            return parameter.value;
+        else if(parameter.mode == MODE_POSITION)
+            return m_Memory[parameter.value];
+    }
+
+private:
+    std::string m_ProgramSource;
+    std::vector<int> m_Memory;
+    int m_InstructionPointer;
+    Instruction m_CurrentInstruction;
+    
+};
 
 struct Instruction
 {
+    struct InstructionParameter
+    {
+        int value;
+        int mode;
+    };
     int opcode;
-    int arg1;
-    int arg2;
-    int dest;
+    std::vector<InstructionParameter> params;
+    // int arg1;
+    // int arg2;
+    // int dest;
 };
 
 void CreateProgramMemory(std::vector<int>& memory)
 {
     std::vector<std::string> lines;
-    bool result = GetFileContent("day2/input.txt", lines);
+    bool result = GetFileContent("day5/input.txt", lines);
 
     if(!result)
     {
@@ -45,10 +169,11 @@ void IncreaseProgramCounter(int& programCounter)
 Instruction GetInstruction(const std::vector<int>&memory, const int& programCounter)
 {
     Instruction inst;
-    inst.opcode = memory[programCounter];
-    inst.arg1 = memory[programCounter + 1];
-    inst.arg2 = memory[programCounter + 2];
-    inst.dest = memory[programCounter + 3];
+
+    // inst.opcode = memory[programCounter];
+    // inst.arg1 = memory[programCounter + 1];
+    // inst.arg2 = memory[programCounter + 2];
+    // inst.dest = memory[programCounter + 3];
     return inst;
 }
 
