40ef15306e322e5fd2e40857cdf639cb2f8da395
Daniel Derkacz <mail@daniel-derkacz.de>
2021-02-01 17:29:11 +0100

    day1 part two

--- 2020/day1/day1.hs
+++ 2020/day1/day1.hs
@@ -3,10 +3,14 @@ import System.IO
 
 dayOne :: [Int] -> Int
 dayOne xs = head [ x * y | x <- xs, y <- xs, x + y == 2020 ]
+
+dayOnePartTwo :: [Int] -> Int
+dayOnePartTwo xs = head [ x * y * z | x <- xs, y <-xs , z <- xs, x+y+z == 2020 ]
+
 main = do
     withFile "day1/input.txt" ReadMode (\handle -> do
         content <- hGetContents handle
-        let result = dayOne (map read (words content))
+        let result = dayOnePartTwo (map read (words content))
         putStr (show result)
         )
         
