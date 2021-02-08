733ff978ca5209628acdff7fe7bc39abc1e924c1
Daniel Derkacz <mail@daniel-derkacz.de>
2021-02-08 22:50:00 +0100

    solved all problems of day two

--- 2020/day2/day2.hs
+++ 2020/day2/day2.hs
@@ -12,9 +12,13 @@ possibleRange str = (read minNumber, read maxNumber)
 specialChar :: [Char] -> Char
 specialChar str = ((head . drop 1 . dropWhile (/= ' ')) str)
 
+getSequence :: [Char] -> [Char]
+getSequence = removeWhiteSpace . dropWhile (/= ':')
+  where removeWhiteSpace = (drop 1)
+
 countSpecialChar :: [Char] -> Int
-countSpecialChar str = sum [1 | c <- sequence, c == character]
-  where sequence = (dropWhile (/= ':') str)
+countSpecialChar str = sum [1 | c <- seq, c == character]
+  where seq = getSequence str
         character = specialChar str
 
 isPasswordCorrect :: [Char] -> Bool
@@ -22,13 +26,30 @@ isPasswordCorrect str = count >= minCount && count <= maxCount
   where (minCount, maxCount) = possibleRange str
         count = countSpecialChar str
 
-day2 :: [[Char]] -> Int
-day2 passwords = sum [ 1 | p <- passwords, isPasswordCorrect p ]
+problemOne :: [[Char]] -> Int
+problemOne = countValidPasswords isPasswordCorrect
 
-main = do
+xor :: Bool -> Bool -> Bool
+xor a b = (a || b) && not (a && b)
+
+-- a valid password for the Toboggan Corporate Policies (see problem two description)
+validPasswordTCP :: [Char] -> Bool
+validPasswordTCP str = ((seq !! posOne) == character)  `xor` ((seq !! posTwo) == character)
+  where (posOne, posTwo) = possibleRange str
+        seq = getSequence str
+        character = specialChar str
+
+countValidPasswords :: ([Char] -> Bool) -> [[Char]] -> Int
+countValidPasswords test passwords = sum [ 1 | p <- passwords, test p ]
+
+problemTwo :: [[Char]] -> Int
+problemTwo = countValidPasswords validPasswordTCP 
+
+day2 :: Int -> IO ()
+day2 problem = do
   withFile "day2/input.txt" ReadMode (\handle -> do 
     content <- hGetContents handle 
     let w = lines content
-    let numberCorrectPasswords = day2 w
+    let numberCorrectPasswords = (if problem == 1 then problemOne else problemTwo) w
     putStr (show numberCorrectPasswords)
     )
